3cfb90e48b6a5e97c4b7127b98df25cdc5f4138b
thnbih <22520136@gm.uit.edu.vn>
2024-04-10 15:54:30 +0700

    Update

--- Project_new/src/App.jsx
+++ Project_new/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import DashBoard from './components/DashBoard'
 import './App.css'
 function App() {
   return (
     <>
-      <Header/>
+      <header><Header/></header>
+
+      <div><DashBoard/></div>
       
-      <footer>
+      <div>
         <Footer/>
-      </footer>
+      </div>
       
     </>
   )
