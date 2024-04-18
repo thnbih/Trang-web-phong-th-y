5f5c46f1547a490ff3074b7818280e068e642f76
thnbih <22520136@gm.uit.edu.vn>
2024-04-18 11:49:36 +0700

    Add 52 La

--- Project_new/src/App.jsx
+++ Project_new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import DashBoard from './components/DashBoard'
 import BoiBaiTarot from './components/BoiBaiTarot/BoiBaiTarot'
+import BoiBai52La from './components/BoiBai52La'
 import './App.css'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div><DashBoard/></div>
 
       <div><BoiBaiTarot/></div>
+      <div><BoiBai52La/></div>
       <div>
         <Footer/>
       </div>
---
+++ Project_new/src/components/BoiBai52La/index.js
@@ -0,0 +1,1 @@
+export {default} from './BoiBai52La'
