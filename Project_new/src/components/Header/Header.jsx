d837cb608596888642fcb640ac4ece569a97c840
Trung Kien Luu <kienluu2703@icloud.com>
2024-04-19 13:00:02 +0700

    More responsive header and footer

--- Project_new/src/components/Header/Header.jsx
+++ Project_new/src/components/Header/Header.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 
 function Header() {
-    return (
-        <>
-            <div className={styles.header}>
-                <div className={styles['Logo']}>
-                    <img alt="" className={styles.logo} src="https://static.overlay-tech.com/assets/9b8f6e55-f5d0-42d0-b508-be81c2425e2f.png" />
-                    <a className={styles.anNhien} href='#'>An Nhiên</a>
-                </div>
-                <nav>
+  const [showNav, setShowNav] = useState(false);
 
-                    <div className={styles.dropdown}>
-                        <a href="#">Dịch vụ</a>
-                        <div className={styles.dropdownContent}>
-                            <a href="#">Bói bài tây</a>
-                            <a href="#">Tarot</a>
-                            <a href="#">Xem ngày sinh</a>
-                        </div>
-                    </div>
-                    <a href='#'>Bài viết blog</a>
-                    <a href='#'>Cửa hàng</a>
-                    <a href='#'>LiveStream</a>
-                    <a href='#'>Liên hệ</a>
-                    <a href='#'>Tài khoản</a>
-                    <span></span>
-                </nav>
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.Logo}>
+          <img
+            alt=""
+            className={styles.logo}
+            src="https://static.overlay-tech.com/assets/9b8f6e55-f5d0-42d0-b508-be81c2425e2f.png"
+          />
+          <a className={styles.anNhien} href="#">
+            An Nhiên
+          </a>
+        </div>
+        <button className={styles.navToggle} onClick={toggleNav}>
+          {showNav ? 'Hide Menu' : 'Show Menu'}
+        </button>
+        <nav className={`${showNav ? styles.show : ''}`}>
+          <div className={styles.dropdown}>
+            <a href="#">Dịch vụ</a>
+            <div className={styles.dropdownContent}>
+              <a href="#">Bói bài tây</a>
+              <a href="#">Tarot</a>
+              <a href="#">Xem ngày sinh</a>
             </div>
-        </>
-    );
+          </div>
+          <a href="#">Bài viết blog</a>
+          <a href="#">Cửa hàng</a>
+          <a href="#">LiveStream</a>
+          <a href="#">Liên hệ</a>
+          <a href="#">Tài khoản</a>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Header;
