db6e4a24ff6017beaa7520115875fab946e16ca9
hytong05 <huythongduongpham@gmail.com>
2024-05-11 15:05:22 +0700

    update logic sigup

--- Project_new/src/components/Login/Login.jsx
+++ Project_new/src/components/Login/Login.jsx
@@ -6,44 +6,7 @@ import Detail_Login  from './detail-login';
 import { getUser } from '../auth/auth';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const navigate = useNavigate();
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    try {
-      const response = await axios.post('https://coiboicuchay-be.azurewebsites.net/api/login', {
-        username,
-        password,
-      });
-  
-      // Store user session information in localStorage or sessionStorage
-      const origin = window.location.origin;
-      localStorage.setItem(`${origin}_user`, JSON.stringify(response.data.data.user));
-      localStorage.setItem(`${origin}_access_token`, response.data.data.access_token);
-      localStorage.setItem(`${origin}_refresh_token`, response.data.data.refresh_token);
-
-      navigate('/');
-    } catch (error) {
-      console.error(error.response.data.error);
-      alert(JSON.stringify(error.response.data.error));
-    }
-  };
-  const origin = window.location.origin;
-  const user = getUser(origin);
   const token = getCookie("token");
-
 
   return (
     <>
---
+++ Project_new/src/components/SignUp/detail-sigup.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styles from './SignUp.module.css';
+import { Analytics } from '@vercel/analytics/react';
+import { Helmet } from 'react-helmet';
+
+function Details_SignUp() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      // Send form data to server-side API
+      const response = await axios.post('https://coiboicuchay-be.azurewebsites.net/api/register', {
+        username,
+        password
+      });
+
+      // Handle successful registration
+      console.log(response.data);
+
+      alert(JSON.stringify(response.data.message));
+
+      navigate('/login');
+    } catch (error) {
+      // Handle registration error
+      console.error(error);
+      alert(JSON.stringify(error.response.data));
+    }
+  };
+
+  return (
+    <main>
+      <Helmet>
+        <title>Đăng Ký</title>
+        <meta name="description" content="Đăng ký tài khoản mới để truy cập các tính năng và dịch vụ của chúng tôi." />
+      </Helmet>
+      <form onSubmit={handleSubmit} className={styles.label}>
+        <div className={styles['container']}>
+          <div className={styles['title-container']}>
+            <p>Register</p>
+          </div>
+          <div className={styles['input-container']}>
+            <div className={styles['content-container']}>
+              <div className={styles['content-text']}>
+                <label htmlFor="username">Username:</label>
+              </div>
+              <div className={styles['content-input']}>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['input-container']}>
+            <div className={styles['content-container']}>
+              <div className={styles['content-text']}>
+                <label htmlFor="password">Password:</label>
+              </div>
+              <div className={styles['content-input']}>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['input-container']}>
+            <div className={styles['content-container']}>
+              <div className={styles['content-text']}>
+                <label htmlFor="confirmPassword">Confirm Password:</label>
+              </div>
+              <div className={styles['content-input']}>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['button-container']}>
+            <div className={styles['item-button']}>
+              <button type="submit">Register</button>
+            </div>
+            <div className={styles['item-button']}>
+              <Link to="/login">
+                <button>Go to Login</button>
+              </Link>
+            </div>
+          </div>
+          <Analytics />
+        </div>
+      </form>
+    </main>
+  );
+}
+
+export default Details_SignUp;
