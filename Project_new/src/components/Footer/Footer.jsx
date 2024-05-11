10e6c8e0fdee2426a9a829b1f2c391b0fc0ab2e5
hytong05 <huythongduongpham@gmail.com>
2024-05-11 13:51:24 +0700

    fix footer UI and responsive login, signup

--- Project_new/src/components/Footer/Footer.jsx
+++ Project_new/src/components/Footer/Footer.jsx
@@ -1,59 +1,106 @@
 import React from 'react';
+import { FacebookOutlined, InstagramOutlined, YoutubeOutlined, TwitterOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 import styles from './Footer.module.css';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import logoImg from '../../img/page-logo.png';
 
 function Footer() {
+    const handleFacebookClick = () => {
+        window.open('https://facebook.com', '_blank');
+    };
+
+    const handleInstagramClick = () => {
+        window.open('https://instagram.com', '_blank');
+    };
+
+    const handleYoutubeClick = () => {
+        window.open('https://youtube.com', '_blank');
+    };
+
+    const handleTwitterClick = () => {
+        window.open('https://twitter.com', '_blank');
+    };
+
     return (
-        <div>
-            {/* <Helmet>
-                <meta name="robots" content="noindex, nofollow" />
-            </Helmet> */}
-            <div className={styles.footer}>
-                <div className={styles.NamePage}>
-                    <h1>An Nhiên</h1>
-                </div>
-                <div className={styles.Service}>
-                    <h2 className={styles.VatPham}>Dịch vụ</h2>
-                    <div className={styles.listProduct}>
-                        <ul>
-                            <li>
-                                <Link to="/boi-bai-52-la">Bói bài tây</Link>
-                            </li>
-                            <li>
-                                <Link to="/boi-bai-tarot">Tarot</Link>
-                            </li>
-                            <li>
-                                <Link to="/boi-ngay-sinh">Xem ngày sinh</Link>
-                            </li>
-                            <li>
-                                <Link to="/livestream">Livestream</Link>
-                            </li>
-                        </ul>
+        <>
+            <div className={styles['footer-container']}>
+                <div className={styles['footer-item']}>
+                    <div className={styles['identity-container']}>
+                        <div className={styles['logo-container']}>
+                            <img src={logoImg} alt="logo" />
+                        </div>
+                        <div className={styles['name-container']}>
+                            <p>AN NHIÊN</p>
+                            <div className={styles['slogan-container']}>
+                                <p>An nhiên về tương lai của bạn</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.Information}>
-                    <h2 className={styles.VatPham}>Thông tin</h2>
-                    <div className={styles.listProduct}>
-                        <ul>
-                            <li>Giới thiệu</li>
-                            <li>Chính sách bảo mật</li>
-                            <li>Điều khoản sử dụng</li>
-                            <li>Liên hệ</li>
-                        </ul>
+                <div className={styles['footer-item']}>
+                    <div className={styles['service-container']}>
+                        <div className={styles['service-title']}>
+                            <p>DỊCH VỤ</p>
+                        </div>
+                        <div className={styles['service-item-container']}>
+                            <div className={styles['service-item']}>
+                                <div className={styles['service-text']}>
+                                    <Link to="/boi-bai-52-la"><p>Bói bài Tây</p></Link>
+                                </div>
+                                <div className={styles['service-text']}>
+                                    <Link to="/boi-bai-tarot"><p>Bói bài Tarot</p></Link>
+                                </div>
+                            </div>
+                            <div className={styles['service-item']}>
+                                <div className={styles['service-text']}>
+                                    <Link to="/boi-ngay-sinh"><p>Bói ngày sinh</p></Link>
+                                </div>
+                                <div className={styles['service-text']}>
+                                    <Link to="/livestream"><p>Livestream</p></Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.Help}>                  
-                    <h2 className={styles.VatPham}>Trợ giúp</h2>
-                    <div className={styles.listProduct}>              
-                        <ul>
-                            <li>Hỏi đáp</li>
-                            <li>Chăm sóc khách hàng</li>
-                        </ul>
+                    <div className={styles['social-container']}>
+                        <div className={styles['social-title']}>
+                            <p>LIÊN HỆ</p>
+                        </div>
+                        <div className={styles['social-item-container']}>
+                            <div className={styles['social-item']}>
+                                <Tooltip title="Facebook">
+                                    <span onClick={handleFacebookClick}>
+                                        <FacebookOutlined style={{ fontSize: '50px', color: '#FAACA8', marginRight: '10px', cursor: 'pointer' }} />
+                                    </span>
+                                </Tooltip>
+                            </div>
+                            <div className={styles['social-item']}>
+                                <Tooltip title="Instagram">
+                                    <span onClick={handleInstagramClick}>
+                                        <InstagramOutlined style={{ fontSize: '50px', color: '#FAACA8', marginRight: '10px', cursor: 'pointer' }} />
+                                    </span>
+                                </Tooltip>
+                            </div>
+                            <div className={styles['social-item']}>
+                                <Tooltip title="YouTube">
+                                    <span onClick={handleYoutubeClick}>
+                                        <YoutubeOutlined style={{ fontSize: '50px', color: '#FAACA8', marginRight: '10px', cursor: 'pointer' }} />
+                                    </span>
+                                </Tooltip>
+                            </div>
+                            <div className={styles['social-item']}>
+                                <Tooltip title="Twitter">
+                                    <span onClick={handleTwitterClick}>
+                                        <TwitterOutlined style={{ fontSize: '50px', color: '#FAACA8', marginRight: '10px', cursor: 'pointer' }} />
+                                    </span>
+                                </Tooltip>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
